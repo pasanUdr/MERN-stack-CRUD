13e91a4e80876a6b01daa1c5386c9b0308d1e83e
pasanUdr <117070955+pasanUdr@users.noreply.github.com>
2024-06-18 03:26:06 +0530

    This commit will implemented the adding user part with all it's functionings and successfully saving values to db using axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,68 @@
-// import logo from './logo.svg';
+import { useState } from "react";
 import "./App.css";
+import { FaWindowClose } from "react-icons/fa";
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:4000/"
 
 function App() {
+
+  const [addSection, setAddSection] = useState(false);
+  //for storing inputs temporary
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+  })
+
+  //
+  const handleOnChange = (e) =>{
+    const {value, name} = e.target
+    setFormData((previ)=>{
+      return{
+      ...previ,
+      [name] : value
+    }})
+  }
+
+  //stop refreshing page when clicking submit btn
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    //posting to db through axios
+    const data = await axios.post("/create",formData)
+    console.log(data)
+    //auto close the form after saving data
+    if(data.data.success){
+      setAddSection(false)
+      alert(data.data.message)
+    }
+  };
+
   return (
     <>
       <div className="container">
-        <button className="btn btn-add"> ADD </button>
+        <button className="btn btn-add" onClick={() => setAddSection(true)}>ADD</button>
 
-        <div className="addContainer">
-          <form >
-            <label htmlFor="name">Name:</label>
-            <input type="text" id= "name" name="name"/>
+        {addSection && (
+          <div className="addContainer">
+            <form onSubmit={handleSubmit}>
+              <div className="closeBtn" onClick={() => setAddSection(false)}>
+                <FaWindowClose />
+              </div>
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" onChange={handleOnChange}/>
 
-            <label htmlFor="email">Email:</label>
-            <input type="email" id= "email" name="email"/>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" onChange={handleOnChange}/>
 
-            <label htmlFor="mobile">Mobile:</label>
-            <input type="number" id= "mobile" name="mobile"/>
+              <label htmlFor="mobile">Mobile:</label>
+              <input type="number" id="mobile" name="mobile" onChange={handleOnChange}/>
 
-            <button>Submit</button>
-          </form>
-        </div>
-      
+              <button className="btn">SUBMIT</button>
+            </form>
+          </div>
+        )}
       </div>
-
     </>
   );
 }
